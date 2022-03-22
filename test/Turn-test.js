2af067e168138f4d5531e030f3518beb04a7ecf8
Kal <kal.aalrajhi@gmail.com>
2022-03-22 08:41:19 -0600

    Add test

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -61,4 +61,16 @@ describe('Turn', function() {
     expect(turn2.evaluateGuess()).to.equal(false);
   });
 
+  it('should give feedback', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?',
+    ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, 'What is a comma-separated list of related values?',
+    ['array', 'object', 'function'], 'array');
+
+    const turn1 = new Turn('object', card1);
+    const turn2 = new Turn('function', card2);
+      
+    expect(turn1.giveFeedback()).to.equal('correct!');
+    expect(turn2.giveFeedback()).to.equal('incorrect!');
+  });
 });
