199c8c304aabb7c2952e489cc55a92e30871af85
Kal <kal.aalrajhi@gmail.com>
2022-03-22 07:58:05 -0600

    Add test

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,11 +2,11 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
 describe('Turn', function() {
 
   it('should be a function', function() {
-    const turn = new Turn();
     expect(Turn).to.be.a('function');
   });
 
@@ -14,4 +14,15 @@ describe('Turn', function() {
     const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
   }); 
+
+  it('should be instantiated with a user\'s guess and current flash card', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?',
+    ['object', 'array', 'function'], 'array');
+
+    const userTurn = new Turn('object', card);
+      
+    expect(userTurn.guess).to.equal('object');
+    expect(userTurn.card).to.be.an.instanceof(Card);
+  });
+
 });
