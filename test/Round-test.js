16a6f6956e11d2695b49b2b973059a4a43dcc9eb
Kal <kal.aalrajhi@gmail.com>
2022-03-22 20:19:06 -0600

    Create skeleton for Round test suite and Round class

---
+++ test/Round-test.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Round = require('../src/Round');
+
+describe('Round', function() {
+    beforeEach(function() {
+        // test hook
+    });
+});
