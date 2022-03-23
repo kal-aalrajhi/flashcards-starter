9c174d1442d38f8b894826c4073237d424f5f0ef
Kal <kal.aalrajhi@gmail.com>
2022-03-22 19:41:38 -0600

    Add extra deckSet to hook and DRY up code

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,8 +6,14 @@ const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 
 describe('Deck', function() {
+    let deck;
+    let deckSet1, deckSet2;
     let card1, card2, card3;
     beforeEach(function() {
+        deck = new Deck();
+        deckSet1 = new Deck([card1, card2, card3]);
+        deckSet2 = new Deck([card1, card2]);
+
         card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?',
         ['object', 'array', 'function'], 'object');
         card2 = new Card(2, 'What is a comma-separated list of related values?', 
@@ -21,27 +27,25 @@ describe('Deck', function() {
     });
 
     it('should be an instance of Deck', function() {
-        let deck = new Deck();
-
         expect(deck).to.be.an.instanceof(Deck);
     });
 
     it('should have a default value of an empty array', function() {
-        let deck = new Deck();
-
         expect(deck.cards).to.be.a('array');
         expect(deck.cards).to.have.length(0);
     });
 
-    it('should take in a set of cards', function() {
-        let deck = new Deck([card1, card2, card3]);
+    it('should take in any set of cards', function() {
+        expect(deckSet1.cards).to.be.a('array');
+        expect(deckSet1.cards).to.have.length(3);
 
-        expect(deck.cards).to.be.a('array');
-        expect(deck.cards).to.have.length(3);
+        expect(deckSet2.cards).to.be.a('array');
+        expect(deckSet2.cards).to.have.length(2);
     });
 
     it('should count cards', function() {
-        let deck = new Deck([card1, card2, card3]);
-        expect(deck.countCards()).to.equal(3);
-    })
+        expect(deck.countCards()).to.equal(0);
+        expect(deckSet1.countCards()).to.equal(3);
+        expect(deckSet2.countCards()).to.equal(2);
+    });
 });
