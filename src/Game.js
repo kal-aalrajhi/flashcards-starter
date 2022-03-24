d95b40e2a628edbc8896c6e2e107f143ecbe5bf0
Kal <kal.aalrajhi@gmail.com>
2022-03-23 20:02:52 -0600

    Allow start to create cards

--- src/Game.js
+++ src/Game.js
@@ -1,20 +1,30 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
-const Round = require("./Round");
+const Round = require('./Round');
+const Card = require('./Card');
 
 class Game {
   constructor() {
     this.currentRound = '';
+    this.cards = [];
   }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
------------------------------------------------------------------------`)
+    -----------------------------------------------------------------------`)
   }
 
   printQuestion(round) {
       util.main(round);
+  }
+
+  start() {
+    // Creates cards
+    prototypeQuestions.forEach(question => {
+      let card = new Card(question.id, question.question, question.answers, question.correctAnswer);
+      this.cards.push(card);
+    });
   }
 }
 
--- test/Game-test.js
+++ test/Game-test.js
@@ -6,25 +6,14 @@ const Round = require('../src/Round');
 const Turn = require('../src/Turn');
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
+const data = require('../src/data');
 
 describe('Game', function() {
-    let card1, card2, card3;
-    let deckSet1, deckSet2;
-    let round1, round2;
+    let prototypeQuestions;
     let game;
 
     beforeEach(function() {
-        card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?',
-        ['object', 'array', 'function'], 'object');
-        card2 = new Card(2, 'What is a comma-separated list of related values?', 
-        ['array', 'object', 'function'], 'array');
-        card3 = new Card(3, 'What type of prototype method directly modifies the existing array?',
-        ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
-
-        deckSet1 = new Deck([card1, card2, card3]);
-        deckSet2 = new Deck([card1, card2]);
-        round1 = new Round(deckSet1.cards);
-        round2 = new Round(deckSet2.cards);
+        prototypeQuestions = data.prototypeData;
         game = new Game();
     });
 
@@ -35,10 +24,14 @@ describe('Game', function() {
     it('should be an instance of Game', function() {
         expect(game).to.be.an.instanceof(Game);
     });
-
+    
     it('should keep track of the current round', function() {
         expect(game.currentRound).to.equal('');
     });
-
     
+    it('should have a start method that creates cards', function() {
+        game.start();
+        expect(game.cards).to.have.length(prototypeQuestions.length);
+    });
+
 });
